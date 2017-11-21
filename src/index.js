5e9962f42fee920b521404d0fbcc9fc3176e84d6
Katy Eng <katye610@gmail.com>
2017-11-21 11:15:28 -0500

    Map running

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('In our JS file in SRC')
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2F0eWU2MTAiLCJhIjoiY2phOXRnYm5nMGJpMDJxcXB4MjR5N295MyJ9.XoJY8xER4S-wfLOKhgPZ6Q';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
